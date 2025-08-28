2d648285b4fd18aca8218d6d09b84fcb450c0b72
Sabari-Vijayan <vijayansabari06@gmail.com>
2025-08-28 16:55:25 +0530

    Home page is 99% complete

--- script.js
+++ script.js
@@ -4,3 +4,58 @@ let answer = document.getElementById("pookalam-answer");
 question.addEventListener("click", () => {
     answer.classList.toggle("show-answer");
 });
+
+let q1 = document.getElementById("faq-q1");
+let a1 = document.getElementById("faq-a1");
+
+q1.addEventListener("click", () => {
+    a1.classList.toggle("show-answer");
+})
+
+let q2 = document.getElementById("faq-q2");
+let a2 = document.getElementById("faq-a2");
+
+q2.addEventListener("click", () => {
+    a2.classList.toggle("show-answer");
+})
+
+let q3 = document.getElementById("faq-q3");
+let a3 = document.getElementById("faq-a3");
+
+q3.addEventListener("click", () => {
+    a3.classList.toggle("show-answer");
+})
+
+let q4 = document.getElementById("faq-q4");
+let a4 = document.getElementById("faq-a4");
+
+q4.addEventListener("click", () => {
+    a4.classList.toggle("show-answer");
+})
+
+let q5 = document.getElementById("faq-q5");
+let a5 = document.getElementById("faq-a5");
+
+q5.addEventListener("click", () => {
+    a5.classList.toggle("show-answer");
+})
+
+let q6 = document.getElementById("faq-q6");
+let a6 = document.getElementById("faq-a6");
+
+q6.addEventListener("click", () => {
+    a6.classList.toggle("show-answer");
+})
+
+let q7 = document.getElementById("faq-q7");
+let a7 = document.getElementById("faq-a7");
+
+q7.addEventListener("click", () => {
+    a7.classList.toggle("show-answer");
+})
+
+let button = document.getElementById("link");
+
+button.addEventListener("click", () => {
+    window.location.href = "https://tally.so/r/wzkyrM";
+});
